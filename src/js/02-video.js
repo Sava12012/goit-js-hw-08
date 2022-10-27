792a4746cb3305608db233c58d4ee1c1682b62b1
Dmytro Savych <sava.savych@gmail.com>
2022-10-27 22:24:47 +0300

    complete home work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const videoTime = localStorage.getItem(STORAGE_KEY)
+timeConversion(videoTime)
+
+function startVideo() {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0)
+}
+
+player.on('timeupdate', throttle(onPlay, 500));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+function timeConversion(videoTime) {
+
+  if (videoTime === null) {
+    startVideo()
+  } else {
+    if (confirm("Продовжити перегляд відео?")) {
+      startVideo()
+    } else {
+      localStorage.removeItem(STORAGE_KEY)
+    }
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const STOREG_KEY = "feedback-form-state"
+const formEl = document.querySelector('.feedback-form')
 
+formEl.addEventListener('submit', oneFormMail)
+formEl.addEventListener('input', throttle(onTextInput, 500))
+
+oneFormTextarea()
+
+let formData = JSON.parse(localStorage.getItem(STOREG_KEY)) || {};
+
+function onTextInput(e) {
+  formData[e.target.name] = e.target.value
+
+  localStorage.setItem(STOREG_KEY, JSON.stringify(formData));
+}
+
+function oneFormMail(e) {
+  e.preventDefault();
+
+    console.log(formData);
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STOREG_KEY);
+
+    formData = {}
+  }
+
+function oneFormTextarea() {
+  const mailFormParse = JSON.parse(localStorage.getItem(STOREG_KEY))
+
+  if (mailFormParse) {
+    formEl.elements.email.value = mailFormParse.email || '';
+    formEl.elements.message.value = mailFormParse.message || '';
+  }
+}
